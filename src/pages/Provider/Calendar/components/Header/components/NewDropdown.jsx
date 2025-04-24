23a6cc6ab98a3453841509d941a9e4a4d806e5ab
Aniqa <aanika.chowdhury04@gmail.com>
2025-04-24 22:39:02 +0600

    fixed dropdown closing issue

--- src/pages/Provider/Calendar/components/Header/components/NewDropdown.jsx
+++ src/pages/Provider/Calendar/components/Header/components/NewDropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "../../../../../../components/ui/Button";
 import { Plus } from "lucide-react";
 import Sidebar from "../../SIdebar/Sidebar";
@@ -7,6 +7,7 @@ function NewDropdown() {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenSidebar, setIsOpenSidebar] = useState(false);
   const [sidebarContent, setSiderbarContent] = useState("");
+  const dropdownRef = useRef(null);
 
   const options = [
     { label: "Appointment" },
@@ -22,8 +23,20 @@ function NewDropdown() {
     setSiderbarContent(contentName);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className="relative hidden md:block">
+    <div ref={dropdownRef} className="relative hidden md:block">
       <Button
         size="header"
         className="font-bold gap-1"
@@ -34,21 +47,23 @@ function NewDropdown() {
       </Button>
 
       <div
-        className={`absolute top-6 right-0 mt-2 bg-white shadow-xl rounded z-10 transform transition-all duration-200 ease-out origin-top-right py-2 ${
+        className={`absolute top-6 right-0 mt-2 bg-white shadow-xl rounded  transform transition-all duration-200 ease-out origin-top-right py-2 ${
           isOpen
             ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
             : "opacity-0 scale-95 -translate-y-2 pointer-events-none"
         }`}
       >
-        {options.map((option, index) => (
-          <button
-            key={index}
-            onClick={() => handleSidebar(option?.label)}
-            className="w-full text-left px-4 py-2 hover:bg-gray-100 text-sm"
-          >
-            {option.label}
-          </button>
-        ))}
+        <div>
+          {options.map((option, index) => (
+            <button
+              key={index}
+              onClick={() => handleSidebar(option?.label)}
+              className="w-full text-left px-4 py-2 hover:bg-gray-100 text-sm"
+            >
+              {option.label}
+            </button>
+          ))}
+        </div>
       </div>
       <Sidebar
         isOpen={isOpenSidebar}
