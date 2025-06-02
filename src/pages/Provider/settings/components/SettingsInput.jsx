399a5448eb5cec6eb913eb5015485e381ab58121
Aniqa <aanika.chowdhury04@gmail.com>
2025-06-03 01:29:07 +0600

    designed workspace details page

--- src/pages/Provider/settings/components/SettingsInput.jsx
+++ src/pages/Provider/settings/components/SettingsInput.jsx
@@ -11,9 +11,9 @@ function SettingsInput({ label, defaultValue, isEditMode }) {
           defaultValue={defaultValue}
           className={`${
             isEditMode
-              ? "border rounded border-gray-300 py-1 px-3"
+              ? "border rounded border-gray-300 py-1 px-3 focus:border-primary-500"
               : "border border-transparent"
-          } w-full outline-none focus:border-primary-500`}
+          } w-full outline-none `}
         />
         <div className="absolute top-2 right-3">
           {isEditMode && <IoMdArrowDropdown size={20} />}
